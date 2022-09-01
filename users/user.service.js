cbfd41cd1cb5baa0bca64706697944d94849f228
Bharat Sharma <bs987143@gmail.com>
2022-09-02 01:32:46 +0530

    Initial Commit

---
+++ users/user.service.js
@@ -0,0 +1,88 @@
+﻿const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const jwt = require('jsonwebtoken');
+
+const { secret } = require('config.json');
+
+module.exports = {
+  authenticate,
+  getAll,
+  getById,
+  create,
+  update,
+  delete: _delete,
+};
+
+async function authenticate({ email, password }) {
+  const user = await db.User.scope('withHash').findOne({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.passwordHash)))
+    throw 'Email or password is incorrect';
+
+  // authentication successful
+  const token = jwt.sign({ sub: user.id }, secret, { expiresIn: '7d' });
+  return { ...omitHash(user.get()), token };
+}
+
+async function getAll() {
+  return await db.User.findAll();
+}
+
+async function getById(id) {
+  return await getUser(id);
+}
+
+async function create(params) {
+  // validate
+  if (await db.User.findOne({ where: { email: params.email } })) {
+    throw 'Email "' + params.email + '" is already registered';
+  }
+
+  const user = new db.User(params);
+
+  // hash password
+  user.passwordHash = await bcrypt.hash(params.password, 10);
+
+  // save user
+  await user.save();
+}
+
+async function update(id, params) {
+  const user = await getUser(id);
+
+  // validate
+  const emailChanged = params.email && user.email !== params.email;
+  if (
+    emailChanged &&
+    (await db.User.findOne({ where: { email: params.email } }))
+  ) {
+    throw 'Email "' + params.email + '" is already registered';
+  }
+
+  // hash password if it was entered
+  if (params.password) {
+    params.passwordHash = await bcrypt.hash(params.password, 10);
+  }
+
+  // copy params to user and save
+  Object.assign(user, params);
+  await user.save();
+}
+
+async function _delete(id) {
+  const user = await getUser(id);
+  await user.destroy();
+}
+
+// helper functions
+
+async function getUser(id) {
+  const user = await db.User.findByPk(id);
+  if (!user) throw 'User not found';
+  return user;
+}
+
+function omitHash(user) {
+  const { hash, ...userWithoutHash } = user;
+  return userWithoutHash;
+}
